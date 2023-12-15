16d3c8ef469e81daceb9da784a6ea860e1975da1
Sven Mutsaers <s.mutsaersnl@gmail.com>
2023-12-15 10:57:31 +0100

    database koppelen

---
+++ dist/php/registratie_database.php
@@ -0,0 +1,10 @@
+<?php
+    include 'data.php';
+
+    $conn = new mysqli($servername, $username, $password, $database);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    else {
+        echo "Connected successfully";
+    }
